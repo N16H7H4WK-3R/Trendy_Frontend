eb5af66de161ff931633de056964a5a95d6d74cd
άριος <103638715+N16H7H4WK-3R@users.noreply.github.com>
2023-08-28 01:58:42 +0530

    to be fixed shopcontent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './components/navbar';
-import Carousel from './components/carousel'
+import Carousel from './components/carousel';
+import ShopContent from './components/shop'
 
 function App() {
   return (
     <>
     <NavBar/>
     <Carousel/>
+    <ShopContent/>
     </>
   );
 }
---
+++ src/components/shop.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+
+
+const ShopContent = () => {
+    return (
+        <>
+            <div className="container my-5">
+                <div className="row">
+                    <div className="col-md-4 my-3">
+                        <div className="card" style={{width: '18rem'}}>
+                            <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwonderfulengineering.com%2Fwp-content%2Fuploads%2F2014%2F01%2Fbeautiful-wallpaper-29.jpg&f=1&nofb=1&ipt=ce7e580bbd6143468b5fc746734de31495e39a7740929e63592ec036e4ff5a68&ipo=images" className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <a href="" className="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 my-3">
+                        <div className="card" style={{width: '18rem'}}>
+                            <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwonderfulengineering.com%2Fwp-content%2Fuploads%2F2014%2F01%2Fbeautiful-wallpaper-29.jpg&f=1&nofb=1&ipt=ce7e580bbd6143468b5fc746734de31495e39a7740929e63592ec036e4ff5a68&ipo=images" className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <a href="" className="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 my-3">
+                        <div className="card" style={{width: '18rem'}}>
+                            <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwonderfulengineering.com%2Fwp-content%2Fuploads%2F2014%2F01%2Fbeautiful-wallpaper-29.jpg&f=1&nofb=1&ipt=ce7e580bbd6143468b5fc746734de31495e39a7740929e63592ec036e4ff5a68&ipo=images" className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <a href="" className="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 my-3">
+                        <div className="card" style={{width: '18rem'}}>
+                            <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwonderfulengineering.com%2Fwp-content%2Fuploads%2F2014%2F01%2Fbeautiful-wallpaper-29.jpg&f=1&nofb=1&ipt=ce7e580bbd6143468b5fc746734de31495e39a7740929e63592ec036e4ff5a68&ipo=images" className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <a href="" className="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default ShopContent
