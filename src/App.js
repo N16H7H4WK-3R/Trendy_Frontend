052ffab3776641da1ad6ada3304790888ea8aa9f
άριος <103638715+N16H7H4WK-3R@users.noreply.github.com>
2023-08-28 00:00:05 +0530

    new name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <>
       <nav className="navbar bg-body-tertiary" data-bs-theme="dark">
         <div className="container-fluid">
-          <a className="navbar-brand mx-2">Navbar</a>
+          <a className="navbar-brand mx-2">Trendy</a>
           <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
               <button className="btn btn-outline-success" type="submit">Search</button>
