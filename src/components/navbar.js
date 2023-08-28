ed55011f7ce55af9e7dddba997a4e52472e17d33
άριος <103638715+N16H7H4WK-3R@users.noreply.github.com>
2023-08-29 00:20:15 +0530

    components updated (final)

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,28 +7,28 @@ function CollapsibleExample() {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">Trendy</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="#features">Features</Nav.Link>
                         <Nav.Link href="#pricing">Pricing</Nav.Link>
                         <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.1">One</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">
-                                Another action
+                                Two
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Three</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.4">
-                                Separated link
+                                Four
                             </NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#deets">More deets</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
+                        <Nav.Link href="#deets">More Details</Nav.Link>
+                        <Nav.Link eventKey={2} href="#modes">
+                            Dark Theme
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
--- src/components/shop.js
+++ src/components/shop.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 function GridExample() {
     const rowStyle = {
@@ -15,12 +17,11 @@ function GridExample() {
                     <Col key={idx}>
                         <Card>
                             <Card.Img variant="top" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.bwallpaperhd.com%2Fwp-content%2Fuploads%2F2021%2F01%2FNashPoint.jpg&f=1&nofb=1&ipt=185ed3efaf4ef310136de0581a0a39bd86679f1be2f474651920772e039ef65a&ipo=images" />
-                            <Card.Body>
-                                <Card.Title>Card title</Card.Title>
-                                <Card.Text>
-                                    This is a longer card with supporting text below as a natural
-                                </Card.Text>
-                            </Card.Body>
+                            {/* <Card.Body>
+                            </Card.Body> */}
+                            <Button variant="secondary" size="sm" style={{ borderRadius : '0px' }}>
+                                Add to cart
+                            </Button>
                         </Card>
                     </Col>
                 ))}
