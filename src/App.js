0fd00283acd02fde616747a55817ca319ba64298
άριος <103638715+N16H7H4WK-3R@users.noreply.github.com>
2023-08-31 22:51:15 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import Login from './components/login';
+import Cart from './components/cart';
+// import Login from './components/login';
 // import NavBar from './components/navbar';
 // import Carousel from './components/carousel';
 // import ShopContent from './components/shop';
@@ -12,7 +13,8 @@ function App() {
       <Carousel />
       <ShopContent />
       <Footer /> */}
-      <Login/>
+      {/* <Login/> */}
+      <Cart />
     </>
   );
 }
