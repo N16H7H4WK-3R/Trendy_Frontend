2722e10e4c7d72eaac3e903ec8404c8860501ae2
άριος <103638715+N16H7H4WK-3R@users.noreply.github.com>
2023-09-04 19:10:58 +0530

    Alert/Toast Updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
-import ShopDetails from './components/productDetails';
-// import NavBar from './components/navbar';
-// import Carousel from './components/carousel';
-// import ShopContent from './components/shop';
-// import Footer from './components/footer';
+// import ShopDetails from './components/productDetails';
+import NavBar from './components/navbar';
+import Carousel from './components/carousel';
+import ShopContent from './components/shop';
+import Footer from './components/footer';
 // import Cart from './components/cart';
 // import Login from './components/login';
 
 function App() {
   return (
     <>
-      {/* <NavBar />
+      <NavBar />
       <Carousel />
       <ShopContent />
-      <Footer /> */}
+      <Footer />
       {/* <Login />
       <Cart /> */}
-      <ShopDetails/>
+      {/* <ShopDetails/> */}
     </>
   );
 }
