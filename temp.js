b5e6bd40b4d223a83fab86c4292bf0fc9e48384b
άριος <103638715+N16H7H4WK-3R@users.noreply.github.com>
2023-09-04 20:35:11 +0530

    Updated Icons in Navbar

---
+++ temp.js
@@ -0,0 +1,27 @@
+<div class="col-md-5 my-auto">
+    <ul class="nav justify-content-end">
+
+        <li class="nav-item">
+            <a class="nav-link" href="/">
+                <i class="fa fa-shopping-cart"></i> Cart (0)
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/">
+                <i class="fa fa-heart"></i> Wishlist (0)
+            </a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="fa fa-user"></i> Username
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="/"><i class="fa fa-user"></i> Profile</a></li>
+                <li><a class="dropdown-item" href="/"><i class="fa fa-list"></i> My Orders</a></li>
+                <li><a class="dropdown-item" href="/"><i class="fa fa-heart"></i> My Wishlist</a></li>
+                <li><a class="dropdown-item" href="/"><i class="fa fa-shopping-cart"></i> My Cart</a></li>
+                <li><a class="dropdown-item" href="/"><i class="fa fa-sign-out"></i> Logout</a></li>
+            </ul>
+        </li>
+    </ul>
+</div>
