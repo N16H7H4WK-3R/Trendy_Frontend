d1dd1497fdffbdae8a5ea398a2d2c2484bf9b168
άριος <103638715+N16H7H4WK-3R@users.noreply.github.com>
2023-09-05 23:54:57 +0530

    Fixed Signin button Issue

--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     MDBContainer,
     MDBCol,
@@ -13,45 +13,101 @@ function Login() {
     const buttonStyle = {
         width: '100%',
         height: '50px',
+        overflow: 'hidden',
+    };
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isValidEmail, setIsValidEmail] = useState(true);
+
+    const validateEmail = (value) => {
+        // Regular expression for basic email validation
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        setIsValidEmail(emailRegex.test(value));
+    };
+
+    const handleEmailChange = (e) => {
+        const value = e.target.value;
+        setEmail(value);
+        validateEmail(value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Check if email is valid before proceeding
+        if (!isValidEmail) {
+            alert('Please enter a valid email address.');
+            return;
+        }
+
+        // Continue with form submission
+        console.log('Email:', email);
+        console.log('Password:', password);
     };
 
     return (
-        <div className="login-container">
-            <MDBContainer fluid className="p-3 my-5">
-                <MDBRow>
-                    <MDBCol col='10' md='6'>
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" className="img-fluid" alt="Login" />
-                    </MDBCol>
-                    <MDBCol col='4' md='6'>
-                        <MDBInput wrapperClass='mb-4' label='Email address' id='emailFormControlLg' type='email' size="lg" />
-                        <MDBInput wrapperClass='mb-4' label='Password' id='passwordFormControlLg' type='password' size="lg" />
+        <>
+            <div className="login-container" style={{ maxWidth: '100' }}>
+                <MDBContainer fluid className="p-3 my-5">
+                    <MDBRow>
+                        <MDBCol col='10' md='6'>
+                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" className="img-fluid" alt="Login" />
+                        </MDBCol>
+                        <MDBCol col='4' md='6'>
+                            <form onSubmit={handleSubmit}>
+                                <MDBInput
+                                    wrapperClass='mb-4'
+                                    label='Email address'
+                                    id='emailFormControlLg'
+                                    type='email'
+                                    size="lg"
+                                    value={email}
+                                    onChange={handleEmailChange}
+                                    className={!isValidEmail ? 'is-invalid' : ''}
+                                />
+                                <MDBInput
+                                    wrapperClass='mb-4'
+                                    label='Password'
+                                    id='passwordFormControlLg'
+                                    type='password'
+                                    size="lg"
+                                    value={password}
+                                    onChange={handlePasswordChange}
+                                />
 
-                        <div className="d-flex justify-content-between mx-4 mb-4">
-                            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
-                            <a href="!#">Forgot password?</a>
-                        </div>
+                                <div className="d-flex justify-content-between mx-4 mb-4">
+                                    <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+                                    <a href="!#">Forgot password?</a>
+                                </div>
 
-                        <MDBBtn className="mb-2 wx-100" style={buttonStyle} size="lg">
-                            Sign in
-                        </MDBBtn>
+                                <button className="mb-2 wx-100" style={buttonStyle} size="lg" type="submit">
+                                    Sign in
+                                </button>
+                            </form>
 
-                        <div className="divider d-flex align-items-center my-4">
-                            <p className="text-center fw-bold mx-3 mb-0">OR</p>
-                        </div>
+                            <div className="divider d-flex align-items-center my-4">
+                                <p className="text-center fw-bold mx-3 mb-0">OR</p>
+                            </div>
 
-                        <MDBBtn className="mb-4 wx-100" size="lg" style={{ backgroundColor: '#3b5998', ...buttonStyle }}>
-                            <MDBIcon fab icon="facebook-f" className="mx-2" />
-                            Continue with Facebook
-                        </MDBBtn>
+                            <button className="mb-4 wx-100" size="lg" style={{ backgroundColor: '#3b5998', ...buttonStyle }}>
+                                <MDBIcon fab icon="facebook-f" className="mx-2" />
+                                Continue with Facebook
+                            </button>
 
-                        <MDBBtn className="mb-4 wx-100" size="lg" style={{ backgroundColor: '#55acee', ...buttonStyle }}>
-                            <MDBIcon fab icon="twitter" className="mx-2" />
-                            Continue with Twitter
-                        </MDBBtn>
-                    </MDBCol>
-                </MDBRow>
-            </MDBContainer>
-        </div>
+                            <button className="mb-4 wx-100" size="lg" style={{ backgroundColor: '#55acee', ...buttonStyle }}>
+                                <MDBIcon fab icon="twitter" className="mx-2" />
+                                Continue with Twitter
+                            </button>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
+            </div>
+        </>
     );
 }
 
