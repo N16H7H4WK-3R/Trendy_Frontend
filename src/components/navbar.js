2cf206d93c2fe849190e0a27f80d720af074f214
άριος <103638715+N16H7H4WK-3R@users.noreply.github.com>
2023-09-05 13:25:27 +0530

    Updates

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,36 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function MainNavbar() {
+    const [isComponentLoaded, setIsComponentLoaded] = useState(false);
+
+    const Login = () => {
+        setIsComponentLoaded(true);
+    };
+
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark">
-            <Container>
-                <Navbar.Brand href="#home">Trendy</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <a className="nav-link" href="/">
-                            <i className="fa fa-shopping-cart"></i> Cart (0)
-                        </a>
-                        <a className="nav-link" href="/">
-                            <i className="fa fa-heart"></i> Wishlist (0)
-                        </a>
-                        <NavDropdown title={<><i className="fa fa-user"></i> Username</>} id="collapsible-nav-dropdown">
-                            <li><a className="dropdown-item" href="/"><i className="fa fa-user"></i> Profile</a></li>
-                            <li><a className="dropdown-item" href="/"><i className="fa fa-list"></i> My Orders</a></li>
-                            <li><a className="dropdown-item" href="/"><i className="fa fa-sign-out"></i> Logout</a></li>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <>
+            {isComponentLoaded}
+            <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Trendy</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <a className="nav-link" href="/">
+                                <i className="fa fa-shopping-cart"></i> Cart (0)
+                            </a>
+                            <a className="nav-link" href="/">
+                                <i className="fa fa-heart"></i> Wishlist (0)
+                            </a>
+                            <NavDropdown title={<><i className="fa fa-user"></i> Username</>} id="collapsible-nav-dropdown">
+                                <li><a className="dropdown-item" href="/"><i className="fa fa-user"></i> Profile</a></li>
+                                <li><a className="dropdown-item" href="/"><i className="fa fa-list"></i> My Orders</a></li>
+                                <li><a className="dropdown-item" onClick={Login} href="/login"><i className="fa fa-sign-out"></i> Login</a></li>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
 }
 
