37e0daecfc9b982219e710b64ec85515485d48a3
άριος <103638715+N16H7H4WK-3R@users.noreply.github.com>
2023-09-06 11:48:26 +0530

    Login Authentication

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import styles from './login.module.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import {
     MDBContainer,
     MDBCol,
     MDBRow,
-    MDBBtn,
     MDBIcon,
     MDBInput,
     MDBCheckbox
@@ -19,9 +21,21 @@ function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isValidEmail, setIsValidEmail] = useState(true);
+    const [isValidPassword, setIsValidPassword] = useState(true);
+
+    const showToast = (message, type) => {
+        toast[type](message, {
+            position: 'top-center',
+            autoClose: 3000,
+            hideProgressBar: true,
+            closeOnClick: false,
+            pauseOnHover: false,
+            draggable: true,
+            theme: 'dark',
+        });
+    };
 
     const validateEmail = (value) => {
-        // Regular expression for basic email validation
         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
         setIsValidEmail(emailRegex.test(value));
     };
@@ -33,25 +47,30 @@ function Login() {
     };
 
     const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
+        const value = e.target.value;
+        setPassword(value);
+        setIsValidPassword(value.length >= 8);
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        // Check if email is valid before proceeding
-        if (!isValidEmail) {
-            alert('Please enter a valid email address.');
+        if (!email || !password) {
+            showToast('Please fill the details.', 'error');
             return;
         }
 
-        // Continue with form submission
-        console.log('Email:', email);
-        console.log('Password:', password);
+        if (!isValidEmail || !isValidPassword) {
+            showToast('Please enter a valid email and password.', 'error');
+            return;
+        }
+
+        showToast('Sign In Successful!', 'success');
     };
 
     return (
         <>
+            <ToastContainer />
             <div className="login-container" style={{ maxWidth: '100' }}>
                 <MDBContainer fluid className="p-3 my-5">
                     <MDBRow>
@@ -78,6 +97,7 @@ function Login() {
                                     size="lg"
                                     value={password}
                                     onChange={handlePasswordChange}
+                                    className={!isValidPassword ? 'is-invalid' : ''}
                                 />
 
                                 <div className="d-flex justify-content-between mx-4 mb-4">
@@ -85,7 +105,7 @@ function Login() {
                                     <a href="!#">Forgot password?</a>
                                 </div>
 
-                                <button className="mb-2 wx-100" style={buttonStyle} size="lg" type="submit">
+                                <button className={styles.customButton} style={{ backgroundColor: 'green', ...buttonStyle }} size="lg" type="submit">
                                     Sign in
                                 </button>
                             </form>
@@ -94,12 +114,12 @@ function Login() {
                                 <p className="text-center fw-bold mx-3 mb-0">OR</p>
                             </div>
 
-                            <button className="mb-4 wx-100" size="lg" style={{ backgroundColor: '#3b5998', ...buttonStyle }}>
+                            <button className={styles.customButton} size="lg" style={{ backgroundColor: '#3b5998', ...buttonStyle }}>
                                 <MDBIcon fab icon="facebook-f" className="mx-2" />
                                 Continue with Facebook
                             </button>
 
-                            <button className="mb-4 wx-100" size="lg" style={{ backgroundColor: '#55acee', ...buttonStyle }}>
+                            <button className={styles.customButton} size="lg" style={{ backgroundColor: '#55acee', ...buttonStyle }}>
                                 <MDBIcon fab icon="twitter" className="mx-2" />
                                 Continue with Twitter
                             </button>
