30979e85f5b6cfaf118fe9e8d4bf02ae61076098
άριος <103638715+N16H7H4WK-3R@users.noreply.github.com>
2023-09-10 15:41:12 +0530

    updates

--- src/components/wishlist.js
+++ src/components/wishlist.js
@@ -1,21 +1,23 @@
 import React from "react";
-import styles from './componentCss/cart.module.css'
+import styles from './componentCss/cart.module.css';
 
 const Product = () => (
-    <div className="product new my-3" style={{ borderBottom: '1px solid black' }}>
-        <img
-            src="https://source.unsplash.com/800x600/?vest"
-            alt="product"
-            height="80"
-            width="80"
-            style={{ marginBottom: '1rem', marginLeft: '1rem', }}
-        />
-        <div className="mx-5">
-            <span>Product Title</span>
-            <p>Product Description</p>
+    <>
+        <div className="product new my-3" style={{ borderBottom: '1px solid #000000c9' }}>
+            <img
+                src="https://source.unsplash.com/800x600/?vest"
+                alt="product"
+                height="80"
+                width="80"
+                style={{ marginBottom: '1rem', marginLeft: '1rem', }}
+            />
+            <div className="mx-5">
+                <span>Product Title</span>
+                <span>$23.99</span>
+            </div>
+            <div className={styles.favorite}><i className="fa fa-heart"></i></div>
         </div>
-        <label className="price small">$23.99</label>
-    </div>
+    </>
 );
 
 const Wishlist = () => {
