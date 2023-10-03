244c5188ab0177ef440bc375787dcb7023c4af27
N16H7H4WK-3R <aryan014kumar@gmail.com>
2023-10-03 23:17:25 +0530

    optimized code

--- src/components/productDetails.js
+++ src/components/productDetails.js
@@ -7,36 +7,28 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
+const axiosInstance = axios.create({
+    baseURL: 'http://127.0.0.1:8000/services/',
+});
+
+const notify = (message) => {
+    toast.success(message, {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: true,
+        theme: "dark",
+    });
+};
+
 const ShopDetails = () => {
     const { productId } = useParams();
     const [productData, setProductData] = useState(null);
     const navigate = useNavigate();
 
-    const notifyCart = () => {
-        toast.success('Item added to cart !', {
-            position: "top-center",
-            autoClose: 3000,
-            hideProgressBar: true,
-            closeOnClick: false,
-            pauseOnHover: false,
-            draggable: true,
-            theme: "dark",
-        });
-    };
-
-    const notifyFav = () => {
-        toast.success('Item added to favorite ♥', {
-            position: "top-center",
-            autoClose: 3000,
-            hideProgressBar: true,
-            closeOnClick: false,
-            pauseOnHover: false,
-            draggable: true,
-            theme: "dark",
-        });
-    };
-
-    const handleAddToCartClick = async (productData) => {
+    const handleAddToCartClick = async () => {
         try {
             const token = sessionStorage.getItem('token');
 
@@ -46,21 +38,19 @@ const ShopDetails = () => {
             }
 
             const formData = new FormData();
-            formData.append('item_id', 25);
+            formData.append('item_id', productData.productId);
+            formData.append('item_id', productData.productPrice);
+            formData.append('item_id', productData.imageUrl);
             formData.append('quantity', 1);
 
-            const response = await axios.post(
-                'http://127.0.0.1:8000/services/cart/',
-                formData,
-                {
-                    headers: {
-                        Authorization: `Token ${token}`,
-                    },
-                }
-            );
+            const response = await axiosInstance.post('cart/', formData, {
+                headers: {
+                    Authorization: `Token ${token}`,
+                },
+            });
 
             if (response.status === 201) {
-                notifyCart();
+                notify('Item added to cart !');
             } else {
                 console.error('Error adding item to cart:', response);
             }
@@ -73,13 +63,8 @@ const ShopDetails = () => {
         }
     };
 
-
-    const handleAddToFavClick = () => {
-        notifyFav();
-    };
-
     useEffect(() => {
-        axios.get(`http://127.0.0.1:8000/services/detail/${productId}/`)
+        axiosInstance.get(`detail/${productId}/`)
             .then(response => {
                 setProductData(response.data);
             })
@@ -90,36 +75,20 @@ const ShopDetails = () => {
 
     return (
         <>
-            {productData && (
+            {productData ? (
                 <div className={styles.container}>
                     <Carousel className={styles.carousel}>
-                        {productData.imageUrl1 && (
-                            <Carousel.Item>
-                                <img
-                                    className={styles.images}
-                                    src={productData.imageUrl1}
-                                    alt="First slide"
-                                />
-                            </Carousel.Item>
-                        )}
-                        {productData.imageUrl2 && (
-                            <Carousel.Item>
-                                <img
-                                    className={styles.images}
-                                    src={productData.imageUrl2}
-                                    alt="Second slide"
-                                />
-                            </Carousel.Item>
-                        )}
-                        {productData.imageUrl3 && (
-                            <Carousel.Item>
-                                <img
-                                    className={styles.images}
-                                    src={productData.imageUrl3}
-                                    alt="Third slide"
-                                />
-                            </Carousel.Item>
-                        )}
+                        {[1, 2, 3].map((index) => (
+                            productData[`imageUrl${index}`] && (
+                                <Carousel.Item key={index}>
+                                    <img
+                                        className={styles.images}
+                                        src={productData[`imageUrl${index}`]}
+                                        alt={`Slide ${index}`}
+                                    />
+                                </Carousel.Item>
+                            )
+                        ))}
                     </Carousel>
                     <div className={styles.product}>
                         <h1 style={{ color: 'black' }}>{productData.productTitle}</h1>
@@ -127,11 +96,11 @@ const ShopDetails = () => {
                         <p className={styles.desc}>{productData.productDescription}</p>
                         <div className={styles.buttons}>
                             <button className={styles.add} onClick={handleAddToCartClick}>Add to Cart</button>
-                            <button className={styles.like} onClick={handleAddToFavClick}><span>♥</span></button>
+                            <button className={styles.like} onClick={() => notify('Item added to favorite ♥')}><span>♥</span></button>
                         </div>
                     </div>
                 </div>
-            )}
+            ) : null}
             <ToastContainer />
         </>
     );
