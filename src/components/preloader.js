8e61ed687156ee8cf890befd20ae9f078d44d50c
N16H7H4WK-3R <aryan014kumar@gmail.com>
2023-10-10 23:20:12 +0530

    updates

--- src/components/preloader.js
+++ src/components/preloader.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loader = () => {
     return (
         <div className="loader-container">
-            <div className="loader"></div>
+            <div class="ball"></div>
         </div>
     );
 };
